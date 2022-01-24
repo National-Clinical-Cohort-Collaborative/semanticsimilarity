22d651b127ef6ec3072b71de43d891a327747a76
justinreese@lbl.gov <justinreese@lbl.gov>
2022-01-24 17:32:00 +0000

    Build Commit

---
+++ src/test/__init__.py
@@ -0,0 +1,3 @@
+# This file ensures that this directory will be published as part of this library
+# Find out more about regular packages here:
+# https://docs.python.org/3/reference/import.html#regular-packages
---
+++ src/test/test_reality.py
@@ -0,0 +1,6 @@
+from unittest import TestCase
+
+class TestReality(TestCase):
+
+    def test_reality(self):
+        self.assertTrue(2 + 2 == 5)
