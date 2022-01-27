9c6a29f0eecfdf279e92c19494be18a3d44b9aea
justinreese@lbl.gov <justinreese@lbl.gov>
2022-01-27 19:18:37 +0000

    Build Commit

--- src/clustering/hpo_ensmallen_parser.py
+++ src/clustering/hpo_ensmallen_parser.py
@@ -21,7 +21,7 @@ class Hpo2EnsmallenParser:
             directed=True,
             sources_column="subject",
             destinations_column="object",
-            edge_separator=","
+            edge_separator="\t"
             )
 
     @property
