65404a4cd90b3d2899b70eb23db1d544e30d0ce0
justinreese@lbl.gov <justinreese@lbl.gov>
2022-01-27 19:39:55 +0000

    add meaningful test

--- src/clustering/hpo_ensmallen.py
+++ src/clustering/hpo_ensmallen.py
@@ -1,14 +1,15 @@
 class HpoEnsmallen:
     """A class to keep track of things like information content and maps for doing sem sim
     """
+
     def __init__(self, g):
         self.graph = g
 
-    # TODO:
     # get all descendents
-    def get_descendents(self):
-        return None
+    def get_descendents(self, hpo_term) -> list:
+        print(dir(self.graph))
+        return []
 
     # get all ancestors
-    def get_ancestors(self):
-        return None
+    def get_ancestors(self, hpo_term) -> list:
+        return []
