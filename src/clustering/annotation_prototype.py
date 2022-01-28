a5494879f96f4b5e81ce35871a4298d32dd9e2eb
palantir <palantir@medallion-pgb-stemma-2.ops.palantir.local>
2022-01-28 12:43:30 +0000

    dirty working state

--- src/clustering/annotation_prototype.py
+++ src/clustering/annotation_prototype.py
@@ -1,10 +1,1 @@
-# from pyspark.sql import functions as F
-from transforms.api import transform_df, Input, Output
 
-
-@transform_df(
-    Output("/UNITE/[RP-2AE058] [N3C Operational] Machine-learning resources for N3C/Semantic_similarity_clustering/src/clustering/annotation_prototype"),
-    source_df=Input("SOURCE_DATASET_PATH"),
-)
-def compute(source_df):
-    return source_df
