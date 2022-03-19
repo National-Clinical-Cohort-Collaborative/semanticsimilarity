85b40197f2280a68bf677477ca58d2afd9445ae3
ptdocker <ptdocker@stemma-work-state-256014040-73e2f3313dab4d6ab9377e1b18dfe3f48fh>
2022-03-19 16:26:12 +0000

    dirty working state

--- src/test/test_phenomizer.py
+++ src/test/test_phenomizer.py
@@ -94,7 +94,7 @@ class TestPhenomizer(TestCase):
         p = Phenomizer(self.resnik.get_mica_d())
         ss1 = 
         ss2 = p.similarity_score(patientB, patientA)
-        self.assertEqual(ss1, ss2)
+        self.assertEqual(p.similarity_score(patientA, patientB), ss2)
 
     def test_phenomizer_leaf_against_leaf(self):
         # make two patients
