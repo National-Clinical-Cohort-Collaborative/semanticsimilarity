afc6ea4e306c62e52fba02511ad6ea35c0e3c4e1
ptdocker <ptdocker@stemma-work-state-256014040-73e2f3313dab4d6ab9377e1b18dfe3kvtlg>
2022-03-19 16:15:09 +0000

    dirty working state

--- src/test/test_phenomizer.py
+++ src/test/test_phenomizer.py
@@ -93,7 +93,7 @@ class TestPhenomizer(TestCase):
 
         p = Phenomizer(self.resnik.get_mica_d())
         ss1 = p.similarity_score(self.patientA, self.patientB)
-        ss2 = p.similarity_score(self.patientA, self.patientB)
+        ss2 = p.similarity_score(self.patientB, self.patientB)
 
         self.assertTrue(isinstance(ss1, (int, float)))
         self.assertAlmostEquals(ss1, 0)
