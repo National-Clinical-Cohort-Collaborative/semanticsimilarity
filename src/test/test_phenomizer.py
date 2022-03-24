9853dc35fa9321fb2b19e8502d5e61f596146f26
justinreese@lbl.gov <justinreese@lbl.gov>
2022-03-24 20:13:42 +0000

    Autogenerated commit message at Mar 24, 2022 at 13:13PM

--- src/test/test_phenomizer.py
+++ src/test/test_phenomizer.py
@@ -254,7 +254,7 @@ class TestPhenomizer(TestCase):
         p = Phenomizer({})
         self.assertTrue(hasattr(p, "patient_to_cluster_similarity"))
 
-    def test_patient_to_cluster_similarity_method_returns_list_with_correct_length(self):
+    def test_patient_to_cluster_similarity_method_returns_list_with_correct_features(self):
         num_of_clusters = [i[0] for i in self.cluster_assignment.select('cluster').distinct().collect()]
         p = Phenomizer(self.resnik.get_mica_d())
         heldout_patient = self.holdout_patients.filter(F.col("patient_id") == 100)
