48317d87e704018d86d3baf3b666ed65f622d0cf
Justin Reese <justaddcoffee@gmail.com>
2022-05-31 16:16:14 -0700

    Trying to fix Sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -36,6 +36,7 @@ extensions = [
     'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
+    'recommonmark'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
